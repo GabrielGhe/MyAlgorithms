df2e51f6978d5aafb35cc7355f10685c96b71bae
Gabriel G <GabrielGhe@users.noreply.github.com>
2014-06-08 15:17:54 -0400

    Update selectionsort.java
    Modifying tabs

--- selectionsort.java
+++ selectionsort.java
@@ -6,12 +6,12 @@ public class SelectionSort {
 	public static List<Integer> selectionSort(List<Integer> lst){
 		//go from last index to first
 		for(int passes= lst.size()-1; passes != -1; --passes){
-			int posOfMax = 0;					//default max is 0
+			int posOfMax = 0;			//default max is 0
 			for(int i=1; i != passes+1; ++i){	//go from ]0, passes]
 				if(lst.get(i) > lst.get(posOfMax)){
-					posOfMax = i;				//find index of biggest
-				}								//and place it at index
-			}									//passes
+					posOfMax = i;		//find index of biggest
+				}				//and place it at index
+			}					//passes
 			swap(lst, passes, posOfMax);
 		}
 		
